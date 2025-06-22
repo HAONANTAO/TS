5ef580bd25433d4d41b495af1c905e35a5defc94
陶浩南 <taoaaron5@gmail.com>
2025-06-22 11:20:46 +1000

    Update Child.tsx

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -6,3 +6,10 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>Child 1 {color}</div>;
 };
+
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>Child 1 {color}</div>;
+};
+
+ChildAsFC.displayName
